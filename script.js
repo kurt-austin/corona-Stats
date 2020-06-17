76a160381ed664f507ceac976ef193df034e06e6
Kieran Lynch <lynch.kieran1@gmail.com>
2020-06-16 21:40:03 -0500

    added more info from our API call and stored them into variables

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ $("#searchBtn").on("click", function (event) {
         method: 'GET',
     }).then(function(response) {
         var confirmedToday = response.covid19.confirmed[0];
+        var county = response.covid19.recordLocation;
+        var deaths = response.covid19.deaths[0];
+        var percentagePositive = ((response.covid19.confirmed[0] / response.covid19.testsPerformed[0]) * 100).toFixed(2) + "%";
+        var populationConfirmed = ((response.covid19.confirmed[0] / response.covid19.totalPopulation) * 100).toFixed(2) + "%";
         console.log(response);
         console.log(confirmedToday);
+        console.log(deaths);
+        console.log(percentagePositive);
+        console.log(populationConfirmed);
     })
 });
 
